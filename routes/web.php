2991ec703841b4bab5518cd616c635686d004ac4
yauheni <yauheninikifarau@gmail.com>
2021-05-18 14:47:59 +0300

    install breeze and fix image path in database

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/test', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
